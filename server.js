bbe67bb427b763be40686d058f1df4bfe8e0f246
DattKillah <34479099+DattKillah@users.noreply.github.com>
2020-08-09 17:03:44 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+var app = express();
+import { readFile } from "fs";
+
+app.get('/listUsers', function (req, res) {
+   readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      console.log( data );
+      res.end( data );
+   });
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
